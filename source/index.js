66e1c26b53cb354285bfd47b4165e3dbce60b172
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-03-01 11:15:01 +0700

    Test connect to rtdb

--- source/index.js
+++ source/index.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+
+const JwtToken = require("../../../../../../CLOUDCODE/Github/oListRepos/NodeJS/oModules/googleAuth/jwt.js").JwtToken;
+const oAxios = require("../../../../../../CLOUDCODE/Github/oListRepos/NodeJS/oModules/oAxios.js");
+const oUtils = require("../../../../../../CLOUDCODE/Github/oListRepos/NodeJS/oModules/oUtils.js");
+
 const InitializeExecuter = () => {
    let options = {};
    let optionsPath = path.join(path.dirname(__filename), "option.executer.json");
@@ -41,3 +46,11 @@ const InitializeSecrets = (options) => {
 };
 const options = InitializeExecuter();
 const secrets = InitializeSecrets(options);
+(async () => {
+   let scopes = "https://www.googleapis.com/auth/firebase.database https://www.googleapis.com/auth/userinfo.email";
+   for (let i = 0; i < secrets.CONFIG.Rtdbs.length; i++) {
+      let rtdb = secrets.CONFIG.Rtdbs[i];
+      let token = await JwtToken(rtdb.client_email, scopes, rtdb.private_key);
+      console.log({ client_email: rtdb.client_email, token });
+   }
+})();
